771f904a8cb3120ced051e22088957c94251a967
huy12345a <Quanghuy.hmtb@gmail.com>
2024-06-06 14:23:01 +0700

    Shinhchunhat

---
+++ src/THtinhShinhchunhat.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class THtinhShinhchunhat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("dien vao chieu dai a :");
+        int a = sc.nextInt();
+        System.out.println("dien vao chieu rong b :");
+        int b = sc.nextInt();
+        System.out.println("S hinh chu nhat la : " + (a*b));
+    }
+}
